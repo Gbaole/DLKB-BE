fafb22572cc8f2d5afb6918bc504cd0b36ef8717
gbao <giabao23479@gmail.com>
2025-05-04 14:24:57 +0700

    commit

--- src/server.js
+++ src/server.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const viewEngine = require('./config/viewEngine');
-const initWebRoutes = require('./route/web');
-const userRouter = require('./route/userRouter');
-const doctorRouter = require('./route/doctorRouter');
-const uploadRouter = require('./route/uploadRouter');
-const cauhoiRouter = require('./route/cauHoiRouter');
-const connectDB = require('./config/connectDB');
-const cors = require('cors');
-const multer = require('multer');
-const path = require('path');
-const Doctor = require('./model/Doctor');
-const cron = require('node-cron');
+const express = require("express");
+const bodyParser = require("body-parser");
+const viewEngine = require("./config/viewEngine");
+const initWebRoutes = require("./route/web");
+const userRouter = require("./route/userRouter");
+const doctorRouter = require("./route/doctorRouter");
+const uploadRouter = require("./route/uploadRouter");
+const cauhoiRouter = require("./route/cauHoiRouter");
+const connectDB = require("./config/connectDB");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const Doctor = require("./model/Doctor");
+const cron = require("node-cron");
 // const moment = require('moment');
-import moment from 'moment-timezone';
-const KhamBenh = require('./model/KhamBenh');
+import moment from "moment-timezone";
+const KhamBenh = require("./model/KhamBenh");
 
 require("dotenv").config();
 
 let app = express();
 let port = process.env.PORT || 6969;
-const hostname = process.env.HOST_NAME;
 
 connectDB();
 
@@ -32,31 +31,31 @@ connectDB();
 //     })
 // );
 const allowedOrigins = [
-    'http://localhost:3000', // Local development
-    'http://localhost:3002', // Local development
+  "http://localhost:3000", // Local development
+  "http://localhost:3002", // Local development
 ];
 
-app.use(cors({
+app.use(
+  cors({
     origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) { // Dùng includes thay cho indexOf
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+      if (!origin || allowedOrigins.includes(origin)) {
+        // Dùng includes thay cho indexOf
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
     },
     credentials: true,
-}));
-app.options('*', cors()); // Enable preflight requests for all routes
-
-
+  })
+);
+app.options("*", cors()); // Enable preflight requests for all routes
 
 // Config bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Đặt thư mục public/uploads làm public để có thể truy cập
-app.use('/uploads', express.static(path.join(__dirname, './public/uploads')));
-
+app.use("/uploads", express.static(path.join(__dirname, "./public/uploads")));
 
 // Config app
 viewEngine(app);
@@ -67,28 +66,26 @@ app.use("/api/doctor", doctorRouter);
 app.use("/api/doctor", uploadRouter); // Đặt đường dẫn cho upload
 app.use("/api/cauhoi", cauhoiRouter); // Đặt đường dẫn cho upload
 
-
 // Hàm cron job xóa lịch quá hạn
 cron.schedule("*/10 * * * * *", async () => {
-    try {
-        
-        // const today = new Date().toLocaleDateString("en-CA");
-        const today = moment().tz("Asia/Ho_Chi_Minh")
-        const tenGio = today.format("HH:mm")
-        console.log("today: ", today);
-        console.log("tenGio: ", tenGio);
+  try {
+    // const today = new Date().toLocaleDateString("en-CA");
+    const today = moment().tz("Asia/Ho_Chi_Minh");
+    const tenGio = today.format("HH:mm");
+    console.log("today: ", today);
+    console.log("tenGio: ", tenGio);
 
-        // Xóa các lịch thỏa mãn điều kiện
-        const result = await KhamBenh.deleteMany({
-            ngayKhamBenh: { $lt: today.format("DD/MM/YYYY") },  // Ngày hẹn khám bệnh nhỏ hơn hôm nay
-            trangThaiXacNhan: false,       // Chưa được xác nhận
-            // tenGioKham: { $lt: `${tenGio}` }     // nếu giờ hẹn nhỏ hơn giờ hiện tại
-        });
+    // Xóa các lịch thỏa mãn điều kiện
+    const result = await KhamBenh.deleteMany({
+      ngayKhamBenh: { $lt: today.format("DD/MM/YYYY") }, // Ngày hẹn khám bệnh nhỏ hơn hôm nay
+      trangThaiXacNhan: false, // Chưa được xác nhận
+      // tenGioKham: { $lt: `${tenGio}` }     // nếu giờ hẹn nhỏ hơn giờ hiện tại
+    });
 
-        console.log(`Đã xóa ${result.deletedCount} lịch khám bệnh quá hạn.`);
-    } catch (err) {
-        console.error("Có lỗi xảy ra khi xóa lịch khám bệnh:", err);
-    }
+    console.log(`Đã xóa ${result.deletedCount} lịch khám bệnh quá hạn.`);
+  } catch (err) {
+    console.error("Có lỗi xảy ra khi xóa lịch khám bệnh:", err);
+  }
 });
 
 // cron.schedule("*/10 * * * * *", async () => {
@@ -120,20 +117,25 @@ cron.schedule("*/10 * * * * *", async () => {
 
 // Hoặc sử dụng setInterval để kiểm tra thường xuyên
 setInterval(async () => {
-    try {
-        const doctors = await Doctor.find();
+  try {
+    const doctors = await Doctor.find();
 
-        for (const doctor of doctors) {
-            doctor.thoiGianKham = doctor.thoiGianKham.filter(slot => moment(slot.date).isSameOrAfter(moment(), 'day'));
-            await doctor.save();
-        }
-        console.log('Đã tự động xóa các lịch trình cũ thành công!');
-    } catch (error) {
-        console.error('Có lỗi xảy ra khi xóa lịch trình cũ:', error);
+    for (const doctor of doctors) {
+      doctor.thoiGianKham = doctor.thoiGianKham.filter((slot) =>
+        moment(slot.date).isSameOrAfter(moment(), "day")
+      );
+      await doctor.save();
     }
+    console.log("Đã tự động xóa các lịch trình cũ thành công!");
+  } catch (error) {
+    console.error("Có lỗi xảy ra khi xóa lịch trình cũ:", error);
+  }
 }, 1000 * 60 * 1); // 1 phút
 
-
 app.listen(port, () => {
-    console.log("backend nodejs is running on the port:", port, `\n http://localhost:${port}`);
+  console.log(
+    "backend nodejs is running on the port:",
+    port,
+    `\n http://localhost:${port}`
+  );
 });
